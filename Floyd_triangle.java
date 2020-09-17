669c8781772610615290ff44b9d43a4ce069243f
BHARGAVKillermate3004 <65061035+BHARGAVKillermate3004@users.noreply.github.com>
2020-09-17 09:21:02 +0100

    Add files via upload

---
+++ Floyd_triangle.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+ class Floyd_triangle
+{
+    int n;
+    Floyd_triangle()//constructor
+    {
+        n=0;
+    }
+    void input()//for inputof number of rows using Scanner class
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows in the Floyd triangle:");
+        n=sc.nextInt();//inserting user typed value of number of rows
+    }
+    void generate_pattern()//for generating the floyd triangle
+    {
+        int a=1;//for printing the numbers
+        for(int i=1;i<=n;i++)//for number of rows
+        {
+            for(int j=1;j<=i;j++)//for number of values being printed in a row
+            {
+                System.out.print(a+" ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main()
+    {
+        Floyd_triangle obj=new Floyd_triangle();
+        obj.input();
+        obj.generate_pattern();
+    }
+}
+    
